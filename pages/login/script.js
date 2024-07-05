255ef1b41c4e9c2ded12e4ded4b06c387eb35704
Haniel Costa da Silva <87027617+HanielCostaDaSilva@users.noreply.github.com>
2024-07-05 08:49:22 -0300

    pequenos ajustes

--- pages/login/script.js
+++ pages/login/script.js
@@ -6,3 +6,15 @@ function handleEvent(event){
     alert("Seja bem vindo! "+getLoggedInUser().nome+ "!")
     window.location.href = '../../index.html'
 }
+
+document.getElementById('olho').addEventListener('mousedown', function() {
+    document.getElementById('password').type = 'text';
+  });
+  
+  document.getElementById('olho').addEventListener('mouseup', function() {
+    document.getElementById('password').type = 'password';
+  });
+  
+  document.getElementById('olho').addEventListener('mousemove', function() {
+    document.getElementById('password').type = 'password';
+  });
